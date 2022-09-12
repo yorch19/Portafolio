d2cb311538de5c605bb58015a752b9b21f56b72d
Jorge Francisco Baca Sosa <yorch1905@gmail.com>
2022-09-11 22:44:28 -0500

    Update and Firebase deploy

--- src/componentes/workcarddata.js
+++ src/componentes/workcarddata.js
@@ -1,5 +1,7 @@
 import pro1 from "../img/ecommerce.jpg" 
 import pro2 from "../img/libros.jpg" 
+import pro3 from "../img/eventos.jpg" 
+
 
 const ProCardData = [
     {
@@ -12,10 +14,19 @@ const ProCardData = [
     {
         imgsrc: pro2, 
         titulo: "Ecommerce Tienda de Libros",
-        texto: "Plantilla para Ecommerce. Desarrollado con PHP y Bootstrap 4, agrega, edita, elimina y actualiza libros. Incluye sección de administrador y BD.",
+        texto: "Plantilla para Ecommerce. Desarrollado con PHP y Bootstrap, agrega, edita, elimina y actualiza libros. Incluye sección de administrador y BD.",
         ver: 'https://github.com/yorch19',
         codigo: "https://github.com/yorch19/Library"
+    },
+    {
+        imgsrc: pro3, 
+        titulo: "Página Web para Eventos (Front-End)",
+        texto: "Plantilla para eventos. Desarrollado con HTML, Bootstrap y JavaScript. Cuenta con secciones para registro, inicio de sesión y contacto.",
+        ver: 'https://yorch19.github.io/Eventos/',
+        codigo: "https://github.com/yorch19/Eventos"
     }
+    
+
 ];
 
 export default ProCardData;
